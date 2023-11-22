ecaa95efc2c61012f19fb6ee3f04ebd6e37ccc24
KamalJerome <93066516+KamalJerome@users.noreply.github.com>
2023-11-22 22:26:58 +0530

    Update schedalgo.py

--- schedalgo.py
+++ schedalgo.py
@@ -12,19 +12,20 @@ def fcfs(processes):
     waiting_time[0] = 0
 
     for i in range(1, n):
-        waiting_time[i] = waiting_time[i-1] + processes[i-1][1]
+        waiting_time[i] = waiting_time[i-1] + processes[i-1][2]
 
     for i in range(n):
-        turnaround_time[i] = waiting_time[i] + processes[i][1]
+        turnaround_time[i] = waiting_time[i] + processes[i][2]
+        completion_time[i] = turnaround_time[i]
 
     avg_waiting_time = sum(waiting_time) / n
     avg_turnaround_time = round(sum(turnaround_time) / n, 2)
     total_execution_time = calculate_total_execution_time(turnaround_time)
 
     print("\nFCFS Scheduling:")
-    print("Process\tBurst Time\tWaiting Time\tTurnaround Time")
+    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\tCompletion Time")
     for i in range(n):
-        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}")
+        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{processes[i][2]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}\t\t{completion_time[i]}")
 
     print(f"\nAverage Waiting Time: {avg_waiting_time}")
     print(f"Average Turnaround Time: {avg_turnaround_time}")
@@ -33,9 +34,10 @@ def fcfs(processes):
 
 def round_robin(processes, quantum):
     n = len(processes)
-    remaining_time = [process[1] for process in processes]
+    remaining_time = [process[2] for process in processes]
     waiting_time = [0] * n
     turnaround_time = [0] * n
+    completion_time = [0] * n
     time = 0
     queue = []
 
@@ -50,8 +52,9 @@ def round_robin(processes, quantum):
                     queue.append(i)
                 else:
                     time += remaining_time[i]
-                    waiting_time[i] = time - processes[i][1]
+                    waiting_time[i] = time - processes[i][2]
                     turnaround_time[i] = time
+                    completion_time[i] = turnaround_time[i]
                     remaining_time[i] = 0
 
         if done:
@@ -62,9 +65,9 @@ def round_robin(processes, quantum):
     total_execution_time = calculate_total_execution_time(turnaround_time)
 
     print("\nRound Robin Scheduling:")
-    print("Process\tBurst Time\tWaiting Time\tTurnaround Time")
+    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\tCompletion Time")
     for i in range(n):
-        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}")
+        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{processes[i][2]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}\t\t{completion_time[i]}")
 
     print(f"\nAverage Waiting Time: {avg_waiting_time}")
     print(f"Average Turnaround Time: {avg_turnaround_time}")
@@ -73,24 +76,27 @@ def round_robin(processes, quantum):
 
 def sjf(processes):
     n = len(processes)
-    processes.sort(key=lambda x: x[1])
+    processes.sort(key=lambda x: x[2])
     waiting_time = [0] * n
     turnaround_time = [0] * n
+    completion_time = [0] * n
     time = 0
 
     for i in range(n):
         waiting_time[i] = time
-        time += processes[i][1]
+        time += processes[i][2]
         turnaround_time[i] = time
+        completion_time[i] = turnaround_time[i]
 
     avg_waiting_time = sum(waiting_time) / n
     avg_turnaround_time = round(sum(turnaround_time) / n, 2)
     total_execution_time = calculate_total_execution_time(turnaround_time)
 
     print("\nSJF Scheduling:")
-    print("Process\tBurst Time\tWaiting Time\tTurnaround Time")
+    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\tCompletion Time")
     for i in range(n):
-        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}")
+        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{processes[i][2]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}\t\t{completion_time[i]}")
+
 
     print(f"\nAverage Waiting Time: {avg_waiting_time}")
     print(f"Average Turnaround Time: {avg_turnaround_time}")
@@ -99,36 +105,42 @@ def sjf(processes):
 
 def priority_scheduling(processes):
     n = len(processes)
-    processes.sort(key=lambda x: x[1])
+    processes.sort(key=lambda x: x[2])
     waiting_time = [0] * n
     turnaround_time = [0] * n
+    completion_time = [0] * n
     time = 0
 
     for i in range(n):
         waiting_time[i] = time
-        time += processes[i][1]
+        time += processes[i][2]
         turnaround_time[i] = time
+        completion_time[i] = turnaround_time[i]
 
     avg_waiting_time = sum(waiting_time) / n
     avg_turnaround_time = round(sum(turnaround_time) / n, 2)
     total_execution_time = calculate_total_execution_time(turnaround_time)
 
     print("\nPriority Scheduling:")
-    print("Process\tBurst Time\tWaiting Time\tTurnaround Time")
+    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\tCompletion Time")
     for i in range(n):
-        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}")
+        print(f"{processes[i][0]}\t\t{processes[i][1]}\t\t{processes[i][2]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}\t\t{completion_time[i]}")
 
     print(f"\nAverage Waiting Time: {avg_waiting_time}")
     print(f"Average Turnaround Time: {avg_turnaround_time}")
     print(f"Total Execution Time: {total_execution_time}")
     return total_execution_time
 
-# Example usage:
-n=5
-at = [1,2,3]
-bt = [10,5,8]
-processes = [(a,b) for a,b in zip(at,bt)]
-quantum = 2
+# USER INPUT
+n = int(input("Enter the number of processes: "))
+processes = []
+for i in range(n):
+    process_id = input(f"Enter the process ID for process {i + 1}: ")
+    arrival_time = int(input(f"Enter the arrival time for process {process_id}: "))
+    burst_time = int(input(f"Enter the burst time for process {process_id}: "))
+    processes.append((process_id, arrival_time, burst_time))
+
+quantum = int(input("Enter the time quantum for Round Robin scheduling: "))
 
 fcfs_total_execution_time = fcfs(processes)
 round_robin_total_execution_time = round_robin(processes, quantum)
